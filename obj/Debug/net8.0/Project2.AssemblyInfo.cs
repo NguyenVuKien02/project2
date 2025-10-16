d9725a41aac5f29a9788ae69e47b8dda0fccef81
nvk-lesson4 <nguyenvukien02@gmail.com>
2025-10-17 00:24:17 +0700

    update commit

--- obj/Debug/net8.0/Project2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ed30be2e7afdeff060a7beb8c78333bfda97ae8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa38e9fefd8346864a1fd3732941b58c2edd16d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
